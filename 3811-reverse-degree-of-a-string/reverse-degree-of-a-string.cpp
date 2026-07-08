9576dbfdbd081f7883176b279453440f25f3c4d0
Sahil Shrivas <164908897+Sahil-Shrivas@users.noreply.github.com>
2026-07-08 17:40:51 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (78.36%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -3,7 +3,7 @@ public:
     int reverseDegree(string s) {
         int result=0;
         for(int i=0; i<s.length(); i++){
-            int rev=26-(s[i]-'a'+1)+1;
+            int rev=26-(s[i]-'a');
             result=result+rev*(i+1);
         }
         return result;
