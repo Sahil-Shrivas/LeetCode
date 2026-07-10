018c75412ed8d684659ab1b8fc727f1b24763ae4
Sahil Shrivas <164908897+Sahil-Shrivas@users.noreply.github.com>
2026-07-10 11:41:48 +0530

    Time: 15 ms (23.00%) | Memory: 7.9 MB (20.43%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.cpp
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
+    int fun(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        return (fun(n-1)+fun(n-2));
+    }
     int fib(int n) {
-        if(n==0) return 0;      //base case 
-        if(n==1) return 1;      //base case 
-        int ans1=fib(n-1);      //magic 
-        int ans2=fib(n-2);      //magic 
-        return ans1+ans2;       //answer 
+        return fun(n);
     }
 };
