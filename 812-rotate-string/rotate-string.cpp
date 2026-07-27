ad273e63d6d146be26a65d66e67cf485433f023a
Sahil Shrivas <164908897+Sahil-Shrivas@users.noreply.github.com>
2026-07-27 17:28:12 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (98.52%) - LeetSync

---
+++ 812-rotate-string/rotate-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool rotateString(string s, string goal) {
+        for(int i=0; i<s.size(); i++){
+            s.push_back(s[0]);
+            s.erase(0,1);
+            if(s==goal){
+                return true;
+            }
+        }
+        cout << s;
+        return false;
+    }
+};
