22f4e124f8e0be7531efc2a55360800c96ed1e1b
Sahil Shrivas <164908897+Sahil-Shrivas@users.noreply.github.com>
2026-09-02 15:39:33 +0530

    Time: 0 ms (100.00%) | Memory: 30.3 MB (36.43%) - LeetSync

--- 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
+++ 4256-construct-uniform-parity-array-i/construct-uniform-parity-array-i.cpp
@@ -1,6 +1,27 @@
 class Solution {
 public:
     bool uniformArray(vector<int>& nums1) {
-        return true;
+        int hasOdd = false;
+        int hasEven = false;
+
+        for(int i=0; i<nums1.size(); i++){
+            if(i%2 == 0){
+                hasEven = true;
+            }else{
+                hasOdd = true;
+            }
+        }
+
+        if(hasEven && !hasOdd){
+            return true;
+        }
+        if(hasOdd && !hasEven){
+            return true;
+        }
+        if(hasOdd && hasEven){
+            return true;
+        }
+
+        return false;
     }
 };
